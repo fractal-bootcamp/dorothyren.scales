1003a7156647308275fe1d6c1d2886517e166adc
dxren <dorothy.x.ren@gmail.com>
2024-07-10 17:44:05 -0400

    React2 scale complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,63 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+
+interface InputFieldProps {
+  placeholder: string;
+  value: string;
+  //the onChange prop is a function that takes in an argument structured as a string and returns nothing(void)
+  onChange: (value: string) => void;
+  error?: string
+}
+
+//this is a function declaration in TS 
+//({placeholder}) - this is object destructuring in function parameters. the function expects an object as an argument and destructuring to extract placeholder from that object. 
+// :{placeholder:string} is saying the function expects an object with a placeholder property of type string
+function InputField(props: InputFieldProps) {
+  const { placeholder, value, onChange, error } = props
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        style={{ borderColor: error ? 'red' : undefined }}
+      />
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+  )
+}
+function NameDisplay({ name }: { name: string }) {
+  return <p> Hello, {name}!</p>
+}
 
 function App() {
-  const [input, setInput] = useState('')
-  const [output, setOutput] = useState('')
+  const [name, setName] = useState('')
+  const [submittedName, setSubmittedName] = useState('')
+  const [error, setError] = useState('')
 
-  useEffect(() => {
-    console.log('button clicked')
-  }, [output])
+  const handleSubmit = () => {
+    if (name.trim() === '') {
+      setError('name cannot be empty')
+    } else {
+      setSubmittedName(name)
+      setError('')
+    }
+  }
 
   return (
     <>
-      <input type="text" onChange={(e) => setInput(e.target.value)}></input>
-      <button type="submit" onClick={() => setOutput(input)}>Submit</button>
-      {output}
+      <h1>Props and Controlled Components</h1>
+      <InputField
+        placeholder="hello"
+        //
+        value={name}
+        //when the input field changes, the onchange event fires. this will call on the onChange prop
+        //the onChange prop is set to setName, and this updates the name state in App
+        onChange={setName}
+        error={error}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      {submittedName && <NameDisplay name={submittedName} />}
     </>
   )
 }
